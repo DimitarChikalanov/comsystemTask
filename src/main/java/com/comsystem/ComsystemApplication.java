e6db95ef64be4d4469fa82572cfccaba3eacaa2a
dimitar <dimitur.chikalanov@gmail.com>
2025-06-12 14:52:35 +0300

    intial commit

---
+++ src/main/java/com/comsystem/ComsystemApplication.java
@@ -0,0 +1,13 @@
+package com.comsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComsystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComsystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/comsystem/ComsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComsystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
